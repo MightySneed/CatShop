e774547baca126fbe7a91ba392938d399dacf535
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-10-02 15:55:31 +0100

    cats are generated from images with corresponding sex, breed, name and price

--- Cats4Life/src/App.jsx
+++ Cats4Life/src/App.jsx
@@ -1,3 +1,9 @@
+
+
+import './App.css';
+import { useState, useEffect } from 'react';
+import { faker } from '@faker-js/faker';
+
 function App() {
   const [items, setItems] = useState([]);
   const [message, setMessage] = useState("");
@@ -6,6 +12,53 @@ function App() {
   useEffect(() => {
     setCompletedCount(items.filter(item => item.completed).length);
   }, [items]);
+
+  // Fetching Data (Cat)
+  async function fetchCat() {
+    const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+    const CatData = await res.json()
+    console.log(CatData)
+    const UpCat = CatData.map((Cat)=>{
+      return {
+        ...Cat,
+        CatBreed: faker.animal.cat(),
+        CatPrice: faker.commerce.price({min: 100, max: 1200}),
+        CatSex: faker.person.sex(), 
+        CatName: faker.person.firstName(),
+
+
+      }
+    })
+    console.log(UpCat)
+  }
+  useEffect(()=>{
+    fetchCat() 
+  },[])
+
+  // //Faker introduced here
+  // const CatBreed = faker.animal.cat() // Generate Cat breed
+  // const CatPrice = faker.commerce.price({min: 100, max: 1200}) // Generate Cat Breed
+  // const CatSex = faker.person.sex(); // Generate Cat Sex
+  // const CatName = faker.person.firstName(CatSex); // Generate Human First name for the cat
+  // //const CatQuirk = 
+  // console.log(CatName)
+  // console.log(CatSex)
+  // console.log(CatBreed)
+  // console.log(CatPrice)
+  
+  //Faker variables -end
+
+  // class Cat {
+  //   constructor(name, price, sex, breed) {
+  //     this.name = CatName
+  //     this.price = CatPrice
+  //     this.sex = CatSex
+  //     this.breed = CatBreed
+  //   }
+  // }
+  // //Cat Constructor
+
+  //Constructor End
 
   const addItem = () => {
     if (message.trim() !== "") {
